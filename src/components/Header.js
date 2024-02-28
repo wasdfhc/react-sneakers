449c5f55566ef6c3d0a90cba272994b6db14afdd
wasdfhc <151555315+wasdfhc@users.noreply.github.com>
2024-02-28 10:45:01 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header(props) {
     <header className="d-flex justify-between align-center">
     <Link to= "/">
       <div className="headerLeft d-flex">
-        <img width={40} height={40} src="/img/logo.png" />
+        <img width={40} height={40} src="img/logo.png" />
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -22,17 +22,17 @@ function Header(props) {
 
       <ul className="d-flex">
         <li onClick={props.onClickCart} className="mr-30 cu-p">
-          <img className="mr-20" width={18} height={18} src="/img/cart.svg" alt="КОрзина" />
+          <img className="mr-20" width={18} height={18} src="img/cart.svg" alt="КОрзина" />
           <span> {totalPrice} руб. </span>
         </li>
         <li className="mr-20 cu-p">
         <Link to="/favorites">
-        <img width={18} height={18} src="/img/heart.svg" alt="Закладки" />
+        <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
         </Link>
         </li>
         <li className="mr-20">
         <Link to="/orders">
-          <img width={18} height={18} src="/img/user.svg" alt="Пользователь" />
+          <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
         </Link>
           
         </li>
